a385189b019d2edf8918106b165041cb18f76767
amnesia <craksyw@gmail.com>
2020-08-30 15:03:41 +0200

    swapped the parser out for ruamel.yaml
    changed from pyyaml to ruamel.yaml which preserves whitespace and
    comments and uses almost exactly the same interface.

--- alacritty-theme.py
+++ alacritty-theme.py
@@ -1,35 +1,31 @@
 #!/usr/bin/env python3
 
-import yaml
-from yaml import CLoader as Loader, CDumper as Dumper
-import argparse
-import sys, os
+"""A small script to change the current alacritty color scheme from the shell"""
 
+#Imports
+from ruamel.yaml import YAML
+import argparse
+import os
+
+yaml = YAML()
 parser = argparse.ArgumentParser('alacritty-theme',
                                  description='Change alacritty color scheme from the shell')
-parser.add_argument('name',
+parser.add_argument('theme',
                     help='the name of the theme to switch to.')
-
 args = parser.parse_args()
-
-print(args.name)
-
 
 alacritty_config_path = os.path.expanduser('~/.config/alacritty/alacritty.yml')
 themes_folder = os.path.expanduser('~/.config/alacritty/themes')
-theme_name = args.name
-
+theme_name = args.theme
 theme_path = os.path.join(themes_folder, theme_name+'.yaml')
-
 
 #read the current config and load the data into a python object
 with open(alacritty_config_path, 'r') as config:
-    config_data = yaml.load(config, Loader=Loader)
+    config_data = yaml.load(config)
 
 #read the theme provided and modify the config yaml
 with open(theme_path, 'r') as theme:
-    theme_data = yaml.load(theme, Loader=Loader)
-
+    theme_data = yaml.load(theme)
     for k,v in theme_data.items():
         if k in config_data:
             config_data[k] = v
